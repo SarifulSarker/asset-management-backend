1fa7c847e42f7443bf03b7f9664a186381aa9d43
shariful <shariful.islam@manush.tech>
2026-05-11 01:17:55 +0600

    ready for render deploy

--- db.js
+++ db.js
@@ -1,26 +1,50 @@
-// db.js
+// // db.js
+// import dotenv from "dotenv";
+// dotenv.config();
+// import pkg from "pg"; // ES Module import
+// const { Pool } = pkg;
+
+// // PostgreSQL connection configuration
+// const dbConfig = {
+//   user: process.env.DATABASE_USER , // DB username
+//   host: process.env.DATABASE_HOST , // DB host
+//   database: process.env.DATABASE_NAME , // Database name
+//   password: process.env.DATABASE_PASSWORD , // DB password
+//   port: Number(process.env.DATABASE_PORT), // Default PostgreSQL port
+// };
+// const pool = new Pool(dbConfig);
+
+// // Test connection
+// pool.connect((err, client, release) => {
+//   if (err) {
+//     return console.error("Error connecting to database", err);
+//   }
+//   console.log("Database connected successfully ✅");
+//   release();
+// });
+
+// export default pool; // ES Module export
+
 import dotenv from "dotenv";
 dotenv.config();
-import pkg from "pg"; // ES Module import
+
+import pkg from "pg";
 const { Pool } = pkg;
 
-// PostgreSQL connection configuration
-const dbConfig = {
-  user: process.env.DATABASE_USER , // DB username
-  host: process.env.DATABASE_HOST , // DB host
-  database: process.env.DATABASE_NAME , // Database name
-  password: process.env.DATABASE_PASSWORD , // DB password
-  port: Number(process.env.DATABASE_PORT), // Default PostgreSQL port
-};
-const pool = new Pool(dbConfig);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
-// Test connection
 pool.connect((err, client, release) => {
   if (err) {
-    return console.error("Error connecting to database", err);
+    console.error("Error connecting to database", err);
+    return;
   }
   console.log("Database connected successfully ✅");
   release();
 });
 
-export default pool; // ES Module export
+export default pool;
--- redis-client.js
+++ redis-client.js
@@ -1,23 +1,32 @@
-// redis-client.js
+
 import dotenv from "dotenv";
 dotenv.config();
 
+// import Redis from "ioredis";
+
+// if (!process.env.REDIS_HOST) {
+//   throw new Error("REDIS_HOST is not defined");
+// }
+
+// const redisClient = new Redis({
+//   host: process.env.REDIS_HOST,
+ 
+//   port: Number(process.env.REDIS_PORT),
+//   db: Number(process.env.REDIS_DB),
+//   family: Number(process.env.REDIS_FAMILY),
+//   //password: env.REDIS_PASS,
+// });
+// redisClient.on("connect", () => console.log("Connected to Redis Server"));
+// redisClient.on("ready", () => console.log("Redis Instance is Ready!"));
+// redisClient.on("error", (err) => console.error("Redis Error:", err));
+
+// export default redisClient;
+
 import Redis from "ioredis";
 
-if (!process.env.REDIS_HOST) {
-  throw new Error("REDIS_HOST is not defined");
-}
+const client = new Redis(process.env.REDIS_URL);
 
-const redisClient = new Redis({
-  host: process.env.REDIS_HOST,
- 
-  port: Number(process.env.REDIS_PORT),
-  db: Number(process.env.REDIS_DB),
-  family: Number(process.env.REDIS_FAMILY),
-  //password: env.REDIS_PASS,
-});
-redisClient.on("connect", () => console.log("Connected to Redis Server"));
-redisClient.on("ready", () => console.log("Redis Instance is Ready!"));
-redisClient.on("error", (err) => console.error("Redis Error:", err));
+client.on("connect", () => console.log("Redis connected"));
+client.on("error", (err) => console.error("Redis error:", err));
 
-export default redisClient;
+export default client;
